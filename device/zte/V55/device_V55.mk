401d2676c9ee1721547296bd4ebeef391f0e9478
ultraviolet <ultravioletnanokitty@gmail.com>
2012-11-19 17:22:21 -0700

    more wifi-only tweaks/trimming

--- device/zte/V55/device_V55.mk
+++ device/zte/V55/device_V55.mk
@@ -358,7 +358,7 @@ PRODUCT_COPY_FILES += \
 #PRODUCT_COPY_FILES += \
 #	device/zte/V55/proprietary/etc/init.qcom.mdm_links.sh:system/etc/init.qcom.mdm_links.sh \
 #	device/zte/V55/proprietary/etc/init.qcom.modem_links.sh:system/etc/init.qcom.modem_links.sh \
-#	device/zte/V55/proprietary/etc/init.qcom.post_boot.sh:system/etc/init.qcom.post_boot.sh \
+	device/zte/V55/proprietary/etc/init.qcom.post_boot.sh:system/etc/init.qcom.post_boot.sh \
 #	device/zte/V55/proprietary/etc/init.qcom.coex.sh:system/etc/init.qcom.coex.sh \
 #	device/zte/V55/proprietary/etc/init.qcom.bt.sh:system/etc/init.qcom.bt.sh \
 #	device/zte/V55/proprietary/etc/init.qcom.fm.sh:system/etc/init.qcom.fm.sh 
