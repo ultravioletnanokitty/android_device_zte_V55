20657e43ca5a49a58e3f29da0894cdf97eeba8ab
ultraviolet <ultravioletnanokitty@gmail.com>
2013-01-19 06:32:33 -0700

    actually removed init.rc

--- device_V55.mk
+++ device_V55.mk
@@ -230,7 +230,6 @@ PRODUCT_COPY_FILES += \
 	device/zte/V55/root/init.target.rc:root/init.target.rc \
 	device/zte/V55/root/init.qcom.sh:root/init.qcom.sh \
 	device/zte/V55/root/init.qcom.rc:root/init.qcom.rc \
-	device/zte/V55/recovery/init.rc:recovery/root/init.rc \
 	device/zte/V55/root/ueventd.V55.rc:root/ueventd.V55.rc \
 	device/zte/V55/root/ueventd.qcom.rc:root/ueventd.qcom.rc \
 
