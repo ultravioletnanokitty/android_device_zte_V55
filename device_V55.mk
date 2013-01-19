03e97111d702a87110cebbe3a07f74f6b376130e
ultraviolet <ultravioletnanokitty@gmail.com>
2013-01-18 21:08:58 -0700

    fixed dumbass mistake

--- device_V55.mk
+++ device_V55.mk
@@ -232,6 +232,7 @@ PRODUCT_COPY_FILES += \
 	device/zte/V55/root/init.qcom.sh:root/init.qcom.sh \
 	device/zte/V55/root/init.qcom.rc:root/init.qcom.rc \
 	device/zte/V55/recovery/init.rc:recovery/root/init.rc \
+	device/zte/V55/root/ueventd.rc:root/ueventd.rc \
 
 # VOLD files
 PRODUCT_COPY_FILES += \
