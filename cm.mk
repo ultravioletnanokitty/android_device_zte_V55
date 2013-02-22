0ed098a8cb4bc30b910992ece614e96701d1ed49
ultraviolet <ultravioletnanokitty@gmail.com>
2013-02-22 03:49:29 -0700

    added bootanimation parameters

--- cm.mk
+++ cm.mk
@@ -8,6 +8,10 @@ $(call inherit-product, device/common/gps/gps_us_supl.mk)
 # Inherit device configuration
 $(call inherit-product, device/zte/V55/device_V55.mk)
 
+# Boot Animation
+TARGET_SCREEN_HEIGHT := 800
+TARGET_SCREEN_WIDTH := 1280
+
 -include vendor/cm/config/common_versions.mk
 
 ## Device identifier. This must come after all inclusions
@@ -16,3 +20,4 @@ PRODUCT_DEVICE := V55
 PRODUCT_MANUFACTURER := ZTE
 PRODUCT_BRAND := ZTE 
 PRODUCT_MODEL := Optik
+PRODUCT_CHARACTERISTICS := tablet
