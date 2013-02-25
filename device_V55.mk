fdf28d3ed0e9da5d81ece5dd3e5903db587377ca
ultraviolet <ultravioletnanokitty@gmail.com>
2013-02-25 06:37:13 -0700

    bluetooth fixes, rearranged firmware

--- device_V55.mk
+++ device_V55.mk
@@ -106,5 +106,25 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/vold.fstab:system/etc/vold.fstab \
     $(LOCAL_PATH)/recovery.fstab:recovery/root/etc/recovery.fstab 
     
+# Firmware	
+PRODUCT_COPY_FILES += \
+	device/zte/V55/firmware/a225_pfp.fw:system/etc/firmware/a225_pfp.fw \
+	device/zte/V55/firmware/a225_pm4.fw:system/etc/firmware/a225_pm4.fw \
+	device/zte/V55/firmware/a225p5_pm4.fw:system/etc/firmware/a225p5_pm4.fw \
+	device/zte/V55/firmware/yamato_pfp.fw:system/etc/firmware/yamato_pfp.fw \
+	device/zte/V55/firmware/yamato_pm4.fw:system/etc/firmware/yamato_pm4.fw \
+    device/zte/V55/firmware/bcm4330.hcd:system/vendor/firmware/bcm4330.hcd \
+    device/zte/V55/firmware/bcm4329.hcd:system/vendor/firmware/bcm4329.hcd
+	device/zte/V55/firmware/dsps_fluid.b00:system/etc/firmware/dsps_fluid.b00 \
+	device/zte/V55/firmware/dsps_fluid.b01:system/etc/firmware/dsps_fluid.b01 \
+	device/zte/V55/firmware/dsps_fluid.b02:system/etc/firmware/dsps_fluid.b02 \
+	device/zte/V55/firmware/dsps_fluid.b03:system/etc/firmware/dsps_fluid.b03 \
+	device/zte/V55/firmware/dsps_fluid.mdt:system/etc/firmware/dsps_fluid.mdt \
+	device/zte/V55/firmware/leia_pfp_470.fw:system/etc/firmware/leia_pfp_470.fw \
+	device/zte/V55/firmware/leia_pm4_470.fw:system/etc/firmware/leia_pm4_470.fw \
+	device/zte/V55/firmware/vidc_1080p.fw:system/etc/firmware/vidc_1080p.fw \
+	device/zte/V55/firmware/apsta_bcm4330_b2.bin:system/etc/wifi/apsta_bcm4330_b2.bin \
+	device/zte/V55/firmware/bcm4330_b2.bin:system/etc/wifi/bcm4330_b2.bin \
+    
 $(call inherit-product-if-exists, vendor/zte/V55/V55-vendor-blobs.mk)
 $(call inherit-product, frameworks/native/build/tablet-7in-hdpi-1024-dalvik-heap.mk)
