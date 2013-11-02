821137ea6aa5dd2b9937c5a89ebc9da92e4b32bb
ultraviolet <ultravioletnanokitty@gmail.com>
2013-11-01 23:01:14 -0600

    Revert "initial 4.2 multi-device work"
    This reverts commit 0353c595448a9acffcb0abc53b6b60562e0a1802.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,12 +1,181 @@
- ####### Board configuration for ZTE V11A ########
- ### Copyright nobody because it's a fucking   ###
- ### Makefile and it's ridiculous to copyright ###
- ### such a document.            --ultraviolet ###
- #################################################
+##### Board configuration for ZTE V55/Optik #####
+### Copyright nobody because it's a fucking   ###
+### Makefile and it's ridiculous to copyright ###
+### such a document.            --ultraviolet ###
+#################################################
 
--include device/zte/smarttab-common/BoardConfigCommon.mk
+TARGET_SPECIFIC_HEADER_PATH := device/zte/V55/include
 
+# Bootloader
+TARGET_NO_BOOTLOADER := true
 TARGET_BOOTLOADER_BOARD_NAME := V55
+
+# Platform
+TARGET_BOARD_PLATFORM := msm8660
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
+
+# Architecture
+TARGET_ARCH := arm
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_ARCH_VARIANT_CPU := cortex-a9
+TARGET_CPU_SMP := true
+ARCH_ARM_HAVE_TLS_REGISTER := true
+
+# Flags
+COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE
+
+# QCOM hardware
+BOARD_USES_QCOM_HARDWARE := true
+BOARD_USES_LEGACY_QCOM := true
+
+# Scorpion optimizations
+TARGET_USE_SCORPION_BIONIC_OPTIMIZATION := true
+TARGET_USE_SCORPION_PLD_SET := true
+TARGET_SCORPION_BIONIC_PLDOFFS := 6
+TARGET_SCORPION_BIONIC_PLDSIZE := 128
+
+# Media
+TARGET_PREBUILT_HEADERS := true
+
+# Audio
+TARGET_QCOM_AUDIO_VARIANT := caf
+COMMON_GLOBAL_CFLAGS += -DQCOM_ACDB_ENABLED -DLEGACY_QCOM_VOICE
+
+# Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_BCM := true
+BOARD_BLUEDROID_VENDOR_CONF := device/zte/V55/bluetooth/V55.txt
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/zte/V55/bluetooth/include
+
+# Wi-Fi
+WPA_SUPPLICANT_VERSION := VER_0_8_X
+BOARD_WPA_SUPPLICANT_DRIVER := WEXT
+#BOARD_HOSTAPD_DRIVER := WEXT
+#BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_bcmdhd
+BOARD_WLAN_DEVICE := bcmdhd
+BOARD_WLAN_DEVICE_REV := bcm4330_b2
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
+WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/dhd.ko"
+WIFI_DRIVER_MODULE_NAME := "dhd"
+WIFI_AP_DRIVER_MODULE_PATH := "/system/lib/modules/dhd.ko"
+WIFI_AP_DRIVER_MODULE_NAME := "dhd"
+WIFI_DRIVER_FW_PATH_STA := "/system/etc/wifi/bcm4330_b2.bin"
+#WIFI_DRIVER_FW_PATH_P2P := "/system/etc/wifi/bcm4330_p2p_b2.bin"
+#WIFI_DRIVER_FW_PATH_AP := "/system/etc/wifi/fw_bcm4330_apsta_b2.bin"
+WIFI_DRIVER_MODULE_ARG := "firmware_path=/system/etc/wifi/bcm4330_b2.bin nvram_path=/persist/zte/wifi/bcm.txt"
+#WIFI_AP_DRIVER_MODULE_ARG := "firmware_path=/system/etc/wifi/fw_bcm4330_apsta_b2.bin nvram_path=/persist/zte/wifi/bcm.txt"
+CONFIG_DRIVER_WEXT := true
+WIFI_BAND := 802_11_ABGN
+BOARD_WEXT_NO_COMBO_SCAN := true
+BOARD_NETWORK_INTERFACES_DIR := "/sys/devices/virtual/net"
+PRODUCT_WIRELESS_TOOLS := true
+
+# Camera
+CAMERA_USES_SURFACEFLINGER_CLIENT_STUB := true
+COMMON_GLOBAL_CFLAGS += -DICS_CAMERA_BLOB -DNO_UPDATE_PREVIEW
+BOARD_NEEDS_MEMORYHEAPPMEM := true
+TARGET_DISABLE_ARM_PIE := true
+BOARD_CAMERA_USE_MM_HEAP := true
+
+# GPS
+TARGET_QCOM_GPS_VARIANT := legacy
+BOARD_USES_QCOM_GPS := true
+BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION := 50000
+BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := V55
+
+# Graphics
+TARGET_QCOM_DISPLAY_VARIANT := legacy
+BOARD_USES_LEGACY_MEDIA := true
+COMMON_GLOBAL_CFLAGS += -DQCOM_NO_SECURE_PLAYBACK -DQCOM_ROTATOR_KERNEL_FORMATS
+USE_OPENGL_RENDERER := true
+TARGET_NO_HW_VSYNC := true
+TARGET_USES_C2D_COMPOSITION := true
+TARGET_USES_PMEM := true
+
+# HDMI
+TARGET_QCOM_HDMI_OUT := true
+TARGET_HAVE_HDMI_OUT := true
+
+# Filesystem
+BOARD_VOLD_MAX_PARTITIONS := 36
+
+# FM Radio
+BOARD_HAVE_FM_RADIO := true
+BOARD_GLOBAL_CFLAGS += -DHAVE_FM_RADIO
+
+# Webkit
+ENABLE_WEBGL := true
+TARGET_FORCE_CPU_UPLOAD := true
+
+# Sensors
+BOARD_INVENSENSE_APPLY_COMPASS_NOISE_FILTER := true
+
+# Bootanimation
+TARGET_BOOTANIMATION_PRELOAD := true
+
+# Partitions
+BOARD_BOOTIMAGE_PARTITION_SIZE := 10485760
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 10485760
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 681574400
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 14285799424
+BOARD_FLASH_BLOCK_SIZE := 131072
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_HAS_NO_MISC_PARTITION := true
+
+# Kernel Config
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom vmalloc=512M kgsl.ptcount=14
+BOARD_KERNEL_BASE := 0x40200000
+BOARD_KERNEL_PAGESIZE := 2048
+TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.4.3
+TARGET_KERNEL_SOURCE := kernel/zte/smarttab
 TARGET_KERNEL_CONFIG := v55_defconfig 
 
-BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := V55
+# SD Card		
+BOARD_HAS_SDCARD_INTERNAL := true
+BOARD_SDEXT_DEVICE := /dev/block/mmcblk1p1
+BOARD_USES_MMCUTILS := true
+
+# Insecure Boot
+ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=0
+ADDITIONAL_DEFAULT_PROPERTIES += persist.service.adb.enable=1
+
+# Vold/UMS Stuff
+BOARD_VOLD_MAX_PARTITIONS := 30
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/usb_mass_storage/lun0/file"
+BOARD_UMS_LUNFILE := "/sys/devices/platform/usb_mass_storage/lun0/file"
+
+# Recovery (TWRP)
+
+#TARGET_PREBUILT_RECOVERY_KERNEL := device/zte/V55/recovery/kernel
+TARGET_PROVIDES_RECOVERY_INIT_RC := true
+TARGET_RECOVERY_INITRC := device/zte/V55/recovery/init.rc
+TARGET_RECOVERY_GUI := true
+DEVICE_RESOLUTION := 1280x800
+BOARD_HAS_NO_SELECT_BUTTON := true
+BOARD_HAS_LARGE_FILESYSTEM := true
+BOARD_CUSTOM_GRAPHICS := ../../../device/zte/V55/recovery/minui/graphics.c
+TW_INTERNAL_STORAGE_PATH := "/data/media"
+TW_INTERNAL_STORAGE_MOUNT_POINT := "data"
+TW_EXTERNAL_STORAGE_PATH := "/storage/sdcard1"
+TW_EXTERNAL_STORAGE_MOUNT_POINT := "sdcard1"
+TW_FLASH_FROM_STORAGE := true 
+TW_DEFAULT_EXTERNAL_STORAGE := true
+RECOVERY_SDCARD_ON_DATA := true
+TW_NO_USB_STORAGE := true
+BOARD_HAS_NO_REAL_SDCARD := true
+
+#SP1_NAME := "modem"
+#SP1_DISPLAY_NAME := "NON-HLOS (modem Firmware)"
+#SP1_BACKUP_METHOD := image
+#SP1_MOUNTABLE := 1
+#SP2_NAME := "persist"
+#SP2_BACKUP_METHOD := image
+#SP2_MOUNTABLE := 1
+#SP3_NAME := "aboot"
+#SP3_DISPLAY_NAME := "Appsboot"
+#SP3_BACKUP_METHOD := image
+#SP3_MOUNTABLE := 1
+
+TARGET_EXTRA_CFLAGS += $(call cc-option,-mtune=cortex-a9,$(call cc-option,-mtune=cortex-a8)) $(call cc-option,-mcpu=cortex-a9,$(call cc-option,-mcpu=cortex-a8))
--- device_V55.mk
+++ device_V55.mk
@@ -1,13 +1,136 @@
- ######### Device Makefile for ZTE V55 ###########
- ### Copyright nobody because it's a fucking   ###
- ### Makefile and it's ridiculous to copyright ###
- ### such a document.            --ultraviolet ###
- #################################################
+####### Device Makefile for ZTE V55/Optik #######
+### Copyright nobody because it's a fucking   ###
+### Makefile and it's ridiculous to copyright ###
+### such a document.            --ultraviolet ###
+#################################################
 
-$(call inherit-product, device/zte/smarttab-common/device_common.mk)
-$(call inherit-product, frameworks/native/build/tablet-7in-hdpi-1024-dalvik-heap.mk)
-$(call inherit-product, device/common/gps/gps_us_supl.mk)
- 
+# Overlay
+DEVICE_PACKAGE_OVERLAYS := $(LOCAL_PATH)/overlay
+
+# Product Tags
+PRODUCT_TAGS += dalvik.gc.type-precise
+
+# DPI
+PRODUCT_AAPT_CONFIG := hdpi xhdpi
+
+# Audio
+PRODUCT_PACKAGES += \
+    audio.a2dp.default \
+    audio_policy.msm8660 \
+    audio.primary.msm8660 \
+    audio_policy.conf \
+    libaudioutils
+
+# Graphics
+PRODUCT_PACKAGES += \
+    copybit.msm8660 \
+    gralloc.msm8660 \
+    hwcomposer.msm8660 \
+    libgenlock \
+    libmemalloc \
+    liboverlay \
+    libqdutils \
+    libtilerenderer
+
+# OMX
+PRODUCT_PACKAGES += \
+    libc2dcolorconvert \
+    libdivxdrmdecrypt \
+    libmm-omxcore \
+    libOmxCore \
+    libOmxVdec \
+    libOmxVenc \
+    libOmxAacEnc \
+    libOmxAmrEnc \
+    libstagefrighthw \
+    libOmxQcelp13Enc \
+    libOmxEvrcEnc \
+    libOmxAmrEnc
+
 # GPS
 PRODUCT_PACKAGES += \
     gps.V55
+
+# Lights
+PRODUCT_PACKAGES += \
+    lights.msm8660
+
+# HDMI
+PRODUCT_PACKAGES += \
+    hdmid
+
+# USB
+PRODUCT_PACKAGES += \
+    com.android.future.usb.accessory
+
+# Filesystem Management Tools
+PRODUCT_PACKAGES += \
+    make_ext4fs \
+    setup_fs
+
+# Feature Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml \
+    frameworks/native/data/etc/android.hardware.camera.xml:system/etc/permissions/android.hardware.camera.xml \
+    frameworks/native/data/etc/android.hardware.camera.autofocus.xml:system/etc/permissions/android.hardware.camera.autofocus.xml \
+    frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+    frameworks/native/data/etc/android.hardware.location.xml:system/etc/permissions/android.hardware.location.xml \
+    frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
+    frameworks/native/data/etc/android.hardware.touchscreen.xml:system/etc/permissions/android.hardware.touchscreen.xml \
+    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
+    frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+    frameworks/native/data/etc/android.software.sip.xml:system/etc/permissions/android.software.sip.xml \
+    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+    packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml
+
+# Input
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/input/matrix-keypad.kl:system/usr/keylayout/matrix-keypad.kl \
+    $(LOCAL_PATH)/input/pmic8058_pwrkey.kl:system/usr/keylayout/pmic8058_pwrkey.kl \
+    $(LOCAL_PATH)/input/atmel-touchscreen.idc:system/usr/idc/atmel-touchscreen.idc \
+    $(LOCAL_PATH)/input/icudt44l.dat:system/usr/icu/icudt44l.dat
+	
+# Config Files
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/config/media_codecs.xml:system/etc/media_codecs.xml \
+    $(LOCAL_PATH)/config/thermald.conf:system/etc/thermald.conf \
+    $(LOCAL_PATH)/config/egl.cfg:system/lib/egl/egl.cfg
+
+# Ramdisk
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/ramdisk/initlogo.rle:root/initlogo.rle \
+    $(LOCAL_PATH)/ramdisk/ueventd.qcom.rc:root/ueventd.qcom.rc \
+    $(LOCAL_PATH)/ramdisk/init.qcom.rc:root/init.qcom.rc \
+    $(LOCAL_PATH)/ramdisk/init.qcom.usb.rc:root/init.qcom.usb.rc \
+    $(LOCAL_PATH)/ramdisk/V55.fstab:root/V55.fstab
+	
+# Fstab Files
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/vold.fstab:system/etc/vold.fstab \
+    $(LOCAL_PATH)/recovery.fstab:recovery/root/etc/recovery.fstab 
+    
+# Firmware	
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/firmware/a225_pfp.fw:system/etc/firmware/a225_pfp.fw \
+    $(LOCAL_PATH)/firmware/a225_pm4.fw:system/etc/firmware/a225_pm4.fw \
+    $(LOCAL_PATH)/firmware/a225p5_pm4.fw:system/etc/firmware/a225p5_pm4.fw \
+    $(LOCAL_PATH)/firmware/yamato_pfp.fw:system/etc/firmware/yamato_pfp.fw \
+    $(LOCAL_PATH)/firmware/yamato_pm4.fw:system/etc/firmware/yamato_pm4.fw \
+    $(LOCAL_PATH)/firmware/bcm4330.hcd:system/etc/firmware/bcm4330.hcd \
+    $(LOCAL_PATH)/firmware/bcm4329.hcd:system/etc/firmware/bcm4329.hcd \
+    $(LOCAL_PATH)/firmware/dsps_fluid.b00:system/etc/firmware/dsps_fluid.b00 \
+    $(LOCAL_PATH)/firmware/dsps_fluid.b01:system/etc/firmware/dsps_fluid.b01 \
+    $(LOCAL_PATH)/firmware/dsps_fluid.b02:system/etc/firmware/dsps_fluid.b02 \
+    $(LOCAL_PATH)/firmware/dsps_fluid.b03:system/etc/firmware/dsps_fluid.b03 \
+    $(LOCAL_PATH)/firmware/dsps_fluid.mdt:system/etc/firmware/dsps_fluid.mdt \
+    $(LOCAL_PATH)/firmware/leia_pfp_470.fw:system/etc/firmware/leia_pfp_470.fw \
+    $(LOCAL_PATH)/firmware/leia_pm4_470.fw:system/etc/firmware/leia_pm4_470.fw \
+    $(LOCAL_PATH)/firmware/vidc_1080p.fw:system/etc/firmware/vidc_1080p.fw \
+    $(LOCAL_PATH)/firmware/apsta_bcm4330_b2.bin:system/etc/wifi/apsta_bcm4330_b2.bin \
+    $(LOCAL_PATH)/firmware/bcm4330_b2.bin:system/etc/wifi/bcm4330_b2.bin
+    
+$(call inherit-product-if-exists, vendor/zte/smarttab/smarttab-vendor-blobs.mk)
+$(call inherit-product, frameworks/native/build/tablet-7in-hdpi-1024-dalvik-heap.mk)
