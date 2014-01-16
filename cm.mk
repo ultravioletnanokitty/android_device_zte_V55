3dc13c48b4e23b3664faea024d148f469e5f08ac
ultraviolet <ultravioletnanokitty@gmail.com>
2014-01-15 21:57:33 -0700

    making things conform to convention

--- cm.mk
+++ cm.mk
@@ -1,11 +1,7 @@
 $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
-
 # Inherit device configuration
-$(call inherit-product, device/zte/V55/device_V55.mk)
+$(call inherit-product, device/zte/V71A/full_V71A.mk)
 
 # Boot Animation
 TARGET_SCREEN_HEIGHT := 800
@@ -14,9 +10,9 @@ TARGET_SCREEN_WIDTH := 1280
 -include vendor/cm/config/common_versions.mk
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := cm_V55
-PRODUCT_DEVICE := V55
+PRODUCT_NAME := cm_V71A
+PRODUCT_DEVICE := V71A
 PRODUCT_MANUFACTURER := ZTE
 PRODUCT_BRAND := ZTE 
-PRODUCT_MODEL := Optik
+PRODUCT_MODEL := SmartTab 7
 PRODUCT_CHARACTERISTICS := tablet
