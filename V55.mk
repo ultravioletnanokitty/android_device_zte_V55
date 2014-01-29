de0ad0fbcc98c45e2caf237d3c40da9945041d5a
ultraviolet <ultravioletnanokitty@gmail.com>
2014-01-29 13:02:41 -0700

    re-unified gps

--- V55.mk
+++ V55.mk
@@ -1,4 +1,4 @@
- ####### Device Makefile for ZTE V55/Optik #######
+ ######### Device Makefile for ZTE V55 ###########
  ### Copyright nobody because it's a fucking   ###
  ### Makefile and it's ridiculous to copyright ###
  ### such a document.            --ultraviolet ###
@@ -7,7 +7,3 @@
 $(call inherit-product, device/zte/smarttab-common/device_common.mk)
 $(call inherit-product, frameworks/native/build/tablet-7in-hdpi-1024-dalvik-heap.mk)
 $(call inherit-product, device/common/gps/gps_us_supl.mk)
- 
-# GPS
-PRODUCT_PACKAGES += \
-    gps.V55
